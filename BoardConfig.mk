524cdaa125583fc200231077ff01fc73a2e8a730
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-02-04 22:37:14 +0100

    move common bootimg to common
    move common KERNEL_CMDLINE and init.*.rc to common git
    for easy maintenance
    
    move device HALs to device
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,5 +16,6 @@ include device/sony/rhine/PlatformConfig.mk
 
 TARGET_BOOTLOADER_BOARD_NAME := D5503
 
-#BOARD_KERNEL_CMDLINE += mem=93M@0 mem=1674M@255M
+BOARD_KERNEL_CMDLINE += androidboot.hardware=amami
 BOARD_KERNEL_CMDLINE += mem=1767M
+
